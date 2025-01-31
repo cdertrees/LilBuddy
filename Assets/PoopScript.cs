06d8450bad910621567bf007a82d668a16419ae7
CDeRosa <cderosa179@gmail.com>
2025-01-31 02:04:21 -0500

    cutscene

---
+++ Assets/PoopScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PoopScript : MonoBehaviour
+{
+    //Please kill my oh my god please kill me (the script not me the person)
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void toMainScene()
+    {
+        SceneManager.LoadScene("TestUI");
+    }
+}
